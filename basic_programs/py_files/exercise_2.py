c44bb51909feaea73072a262b179882ecf9a9474
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2022-02-22 11:49:42 +0100

    Add different exercise2 py files

---
+++ basic_programs/py_files/exercise_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+num_a = 10
+num_b = 20
+
+max_numbers = max(num_a,num_b)
+
+print("The max of {0} and {1} is {2}" .format(num_a, num_b, max_numbers))
---
+++ basic_programs/py_files/exercise_2_input.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+num_a = input("First number: ")
+num_b = input("Second number: ")
+
+# Max of two numbers...
+max_numbers = max(num_a, num_b)
+
+print("The max  of {0} and {1} is {2}" .format(num_a, num_b, max_numbers))
